9c55454ee2ea6afc01bba7bd123286a35ee2562e
Lars Greiss <kufikugel@googlemail.com>
2014-12-02 01:22:37 +0100

    Settings: add back button in actionbar on Status settings
    for more consistence. We add it as drawable due that we still have 3
    other
    places in settings where it is missing.
    
    We will change later....after that we should have worked every
    inconsistence out.
    
    Change-Id: Ie1d3480fecb4a346f3ed320faafc8f86b19bff66

--- src/com/android/settings/deviceinfo/Status.java
+++ src/com/android/settings/deviceinfo/Status.java
@@ -17,6 +17,7 @@
 package com.android.settings.deviceinfo;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.ClipboardManager;
@@ -297,6 +298,12 @@ public class Status extends PreferenceActivity {
         mRes = getResources();
         mUnknown = mRes.getString(R.string.device_info_default);
         mUnavailable = mRes.getString(R.string.status_unavailable);
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            // android.R.id.home will be triggered in onOptionsItemSelected()
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         if (UserHandle.myUserId() == UserHandle.USER_OWNER &&
                 (!isMultiSimEnabled())) {
@@ -738,5 +745,24 @@ public class Status extends PreferenceActivity {
         String icCode = String.format(resources.getString(R.string.ic_code_full,
                 resources.getString(R.string.ic_code)));
         return model + "\n" + icCode;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        final int itemId = item.getItemId();
+        switch (itemId) {
+            case android.R.id.home:
+                goUpToTopLevelSetting(this);
+                return true;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Finish current Activity and go up to the top level Settings.
+     */
+    private static void goUpToTopLevelSetting(Activity activity) {
+        activity.finish();
     }
 }
