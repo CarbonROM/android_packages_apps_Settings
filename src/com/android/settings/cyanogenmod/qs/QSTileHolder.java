1ba77d73ee520ca70b47ba3860216c1058c82e5f
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-03-01 00:22:49 +0100

    Settings - SlimAction as QuickTile (2/2)
    Configure which Slim actions you want to use as tile and then add the
    'SlimAction' tile.
    Single press will switch between Slim actions and long press will
    execute the Slim action. If the tile has only one Slim action, single
    press will execute the action.
    
    Change-Id: I6a01ca065801a2ceabf1b0c21dc9d9c0a0289af2
    
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -205,6 +205,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_media_play";
                 stringId = R.string.qs_music_play_tile;
                 break;
+            case QSConstants.TILE_SLIMACTION:
+                resourceName = "ic_qs_reboot_recovery";
+                stringId = R.string.qs_tile_slimaction;
+                break;
             default:
                 return null;
         }
