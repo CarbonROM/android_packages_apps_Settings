a07230c06dad50220be4204e150979bddfe79b24
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-03-05 19:35:51 +0100

    Settings - QSTile: Reboot/Recovery tile (2/2)
    Change-Id: I0f055caccee90e4c3fde9b63dc43c12dbd4025d9
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -181,6 +181,10 @@ public class QSTileHolder {
                 resourceName ="ic_livedisplay_day";
                 stringId = R.string.live_display_title;
                 break;
+            case QSConstants.TILE_REBOOT:
+                resourceName = "ic_qs_reboot";
+                stringId = R.string.qs_tile_reboot;
+                break;
             default:
                 return null;
         }
