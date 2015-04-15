9ce2abb186c75d90211985f5e884d0a78b26a446
camcory <camcory1@gmail.com>
2015-04-15 16:31:47 +0200

    Settings - Add music tile (2/2)
    Change-Id: Ie36994028ce5c827e31cbc9d17161be5eee2894f

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -196,6 +196,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_appcirclebar_on";
                 stringId = R.string.app_circle_bar_title;
                 break;
+            case QSConstants.TILE_MUSIC:
+                resourceName = "ic_qs_media_play";
+                stringId = R.string.qs_music_play_tile;
+                break;
             case QSConstants.TILE_HEADS_UP:
                 resourceName = "ic_qs_heads_up_on";
                 stringId = R.string.qs_tile_heads_up;
