9dcc4595520cecd97a373480a8482a366fef6bee
Myself5 <myself5@carbonrom.org>
2015-05-27 20:18:14 -0400

    Readd KK Carbon Easteregg

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -261,9 +261,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
             mHits[mHits.length-1] = SystemClock.uptimeMillis();
             if (mHits[0] >= (SystemClock.uptimeMillis()-500)) {
                 Intent intent = new Intent(Intent.ACTION_MAIN);
-                intent.putExtra("is_cm", true);
                 intent.setClassName("android",
-                        com.android.internal.app.PlatLogoActivity.class.getName());
+                        com.android.internal.app.CarbonLogoActivity.class.getName());
                 try {
                     startActivity(intent);
                 } catch (Exception e) {
