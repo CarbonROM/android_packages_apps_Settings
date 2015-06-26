87485c42e9d8e1e5cfac47dc2af31fb82b3af839
STELIX <ssspinni@gmail.com>
2015-06-26 12:08:49 +0200

    Settings - Fix slim actions
    Change-Id: I73d76ac828a45c1d7a49fda081098547884414eb
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- src/com/android/settings/CreateSlimShortcut.java
+++ src/com/android/settings/CreateSlimShortcut.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 SlimRoms
+ * Copyright (C) 2014-2015 SlimRoms
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,6 +26,7 @@ import android.os.Bundle;
 import com.android.internal.util.slim.ActionHelper;
 import com.android.internal.util.slim.DeviceUtils;
 import com.android.internal.util.slim.DeviceUtils.FilteredDeviceFeaturesArray;
+import com.android.internal.util.slim.ImageHelper;
 import com.android.settings.slim.SlimActionShortcut;
 
 import com.android.settings.R;
@@ -67,11 +68,8 @@ public class CreateSlimShortcut extends Activity {
 
                 Drawable icon = ActionHelper.getActionIconImage(
                         CreateSlimShortcut.this, dialogValues[item], null);
-                BitmapDrawable bitmap = (BitmapDrawable) icon;
                 Intent intent = new Intent();
-                if (bitmap != null) {
-                    intent.putExtra(Intent.EXTRA_SHORTCUT_ICON, bitmap.getBitmap());
-                }
+                intent.putExtra(Intent.EXTRA_SHORTCUT_ICON, ImageHelper.drawableToBitmap(icon));
                 intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, dialogEntries[item]);
                 intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, shortcutIntent);
                 setResult(RESULT_OK, intent);
