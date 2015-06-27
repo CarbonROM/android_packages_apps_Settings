b1e928ccc04da063adaf46ed3f465dcbaae7d89f
rogersb11 <brettrogers11@gmail.com>
2015-06-27 17:18:38 -0400

    Slim action floats tile 1/2
    Change-Id: I9c44ac24c37b0659b1859c4f01c45ea7c08f7387

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -216,6 +216,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_sync_on";
                 stringId = R.string.qs_tile_sync;
                 break;
+            case QSConstants.TILE_SLIM_FLOATS:
+                resourceName = "ic_qs_floating_on";
+                stringId = R.string.qs_tile_slim_floats;
+                break;
 
             // Dynamic tiles
             case QSConstants.DYNAMIC_TILE_NEXT_ALARM:
