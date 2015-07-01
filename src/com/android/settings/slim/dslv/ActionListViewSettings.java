0ff494408761467335383684bc634dd710a4f884
STELIX <ssspinni@gmail.com>
2015-07-01 10:43:38 +0200

    Revert "Settings: vector support for action icons"
    This reverts commit 80dafe8315aa28b4c776ad261784d2e4e823d326.

--- src/com/android/settings/slim/dslv/ActionListViewSettings.java
+++ src/com/android/settings/slim/dslv/ActionListViewSettings.java
@@ -33,7 +33,6 @@ import android.content.res.TypedArray;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.VectorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -633,15 +632,10 @@ public class ActionListViewSettings extends ListFragment implements
                         mActivity, ActionHelper.getActionIconImage(mActivity,
                         getItem(position).getClickAction(),
                         iconUri), 48);
+            }
 
-                if ((iconUri.equals(ActionConstants.ICON_EMPTY) &&
-                        getItem(position).getClickAction().startsWith("**")) || (iconUri != null
-                        && iconUri.startsWith(ActionConstants.SYSTEM_ICON_IDENTIFIER))) {
-                    if (d != null) {
-                        d = ImageHelper.getColoredDrawable(d,
-                                getResources().getColor(R.color.dslv_icon_dark));
-                    }
-                }
+            if (iconUri != null && iconUri.startsWith(ActionConstants.SYSTEM_ICON_IDENTIFIER)) {
+                d.setTint(getResources().getColor(R.color.dslv_icon_dark));
             }
             holder.iconView.setImageDrawable(d);
 
