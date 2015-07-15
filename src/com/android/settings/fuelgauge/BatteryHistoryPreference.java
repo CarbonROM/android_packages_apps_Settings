07b5cb5312e9623304a2156814597c8314033654
STELIX <ssspinni@gmail.com>
2015-07-15 15:17:36 +0200

    Revert "settings: dock battery"
    This reverts commit d78e4d98813cdfd554919df4c1ce9fb10ed62c3e.
    
    Change-Id: I91abd52bd278147b0e56115a7abaac435bed06d0

--- src/com/android/settings/fuelgauge/BatteryHistoryPreference.java
+++ src/com/android/settings/fuelgauge/BatteryHistoryPreference.java
@@ -18,10 +18,13 @@ package com.android.settings.fuelgauge;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.BatteryStats;
 import android.preference.Preference;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.settings.R;
 
@@ -33,20 +36,21 @@ import com.android.settings.R;
 public class BatteryHistoryPreference extends Preference {
 
     final private BatteryStats mStats;
-    final private BatteryStats mDockStats;
     final private Intent mBatteryBroadcast;
 
     private boolean mHideLabels;
     private View mLabelHeader;
     private BatteryHistoryChart mChart;
 
-    public BatteryHistoryPreference(Context context, BatteryStats stats, BatteryStats dockStats,
-            Intent batteryBroadcast) {
+    public BatteryHistoryPreference(Context context, BatteryStats stats, Intent batteryBroadcast) {
         super(context);
         setLayoutResource(R.layout.preference_batteryhistory);
         mStats = stats;
-        mDockStats = dockStats;
         mBatteryBroadcast = batteryBroadcast;
+    }
+
+    BatteryStats getStats() {
+        return mStats;
     }
 
     public void setHideLabels(boolean hide) {
@@ -67,7 +71,6 @@ public class BatteryHistoryPreference extends Preference {
         if (mChart == null) {
             // First time: use and initialize this chart.
             chart.setStats(mStats, mBatteryBroadcast);
-            chart.setDockStats(mDockStats, mBatteryBroadcast);
             mChart = chart;
         } else {
             // All future times: forget the newly inflated chart, re-use the
--- src/com/android/settings/fuelgauge/PowerUsageDetail.java
+++ src/com/android/settings/fuelgauge/PowerUsageDetail.java
@@ -85,10 +85,9 @@ public class PowerUsageDetail extends Fragment implements Button.OnClickListener
             SettingsActivity caller, BatteryStatsHelper helper, int statsType, BatteryEntry entry,
             boolean showLocationButton) {
         // Initialize mStats if necessary.
-        final BatteryStats stats = helper.getStats();
-        helper.getDockStats();
+        helper.getStats();
 
-        final int dischargeAmount = stats.getDischargeAmount(statsType);
+        final int dischargeAmount = helper.getStats().getDischargeAmount(statsType);
         Bundle args = new Bundle();
         args.putString(PowerUsageDetail.EXTRA_TITLE, entry.name);
         args.putInt(PowerUsageDetail.EXTRA_PERCENT, (int)
@@ -145,13 +144,15 @@ public class PowerUsageDetail extends Fragment implements Button.OnClickListener
                 if (entry.sipper.drainType == BatterySipper.DrainType.APP) {
                     Writer result = new StringWriter();
                     PrintWriter printWriter = new FastPrintWriter(result, false, 1024);
-                    stats.dumpLocked(caller, printWriter, "", helper.getStatsType(), uid.getUid());
+                    helper.getStats().dumpLocked(caller, printWriter, "", helper.getStatsType(),
+                            uid.getUid());
                     printWriter.flush();
                     args.putString(PowerUsageDetail.EXTRA_REPORT_DETAILS, result.toString());
 
                     result = new StringWriter();
                     printWriter = new FastPrintWriter(result, false, 1024);
-                    stats.dumpCheckinLocked(caller, printWriter, helper.getStatsType(), uid.getUid());
+                    helper.getStats().dumpCheckinLocked(caller, printWriter, helper.getStatsType(),
+                            uid.getUid());
                     printWriter.flush();
                     args.putString(PowerUsageDetail.EXTRA_REPORT_CHECKIN_DETAILS,
                             result.toString());
