84cf1fbfb826b96f1572427497b69dc0d3f3cfd7
Christian Oder <myself5@carbonrom.org>
2016-01-19 17:45:38 +0100

    Add Privacy Topic and implement HAFR Settings
    Also implement our own Metrics Logger
    
    Change-Id: I95a324dbbaa0c900d73ac6931ebb3c8da307ce14

--- src/com/android/settings/search/SearchIndexableResources.java
+++ src/com/android/settings/search/SearchIndexableResources.java
@@ -35,6 +35,7 @@ import com.android.settings.applications.SpecialAccessSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.datausage.DataUsageMeteredSettings;
 import com.android.settings.datausage.DataUsageSummary;
+import com.android.settings.carbon.CRPrivacySettings;
 import com.android.settings.deviceinfo.StorageSettings;
 import com.android.settings.display.ScreenZoomSettings;
 import com.android.settings.fuelgauge.BatterySaverSettings;
@@ -326,6 +327,13 @@ public final class SearchIndexableResources {
                         R.xml.zen_mode_visual_interruptions_settings,
                         ZenModeVisualInterruptionSettings.class.getName(),
                         R.drawable.ic_settings_notifications));
+
+        sResMap.put(CRPrivacySettings.class.getName(),
+                new SearchIndexableResource(
+                        Ranking.getRankForClassName(CRPrivacySettings.class.getName()),
+                        NO_DATA_RES_ID,
+                        CRPrivacySettings.class.getName(),
+                        R.drawable.ic_settings_privacy));
     }
 
     private SearchIndexableResources() {
