c9300bd35c5d548499206b6719ed67fc433be5b0
Joe Maples <joe@frap129.org>
2017-09-08 14:05:31 -0500

    Settings: Integrate CarbonFibers into Settings
    Based on both DU's Nougat and Omni's Oreo implimentations, so
    credits to mazda and maxwen for this.
    
    Android 9 edit: include circular background
    
    Change-Id: Id4e5367bab5826bfc8bced5684fc2e599f8140c4
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

--- src/com/android/settings/dashboard/DashboardFragmentRegistry.java
+++ src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -42,6 +42,8 @@ import com.android.settingslib.drawer.CategoryKey;
 
 import java.util.Map;
 
+import org.carbonrom.carbonfibers.CarbonFibers;
+
 /**
  * A registry to keep track of which page hosts which category.
  */
@@ -101,6 +103,8 @@ public class DashboardFragmentRegistry {
             CategoryKey.CATEGORY_GESTURES);
         PARENT_TO_CATEGORY_KEY_MAP.put(NightDisplaySettings.class.getName(),
             CategoryKey.CATEGORY_NIGHT_DISPLAY);
+        PARENT_TO_CATEGORY_KEY_MAP.put(CarbonFibers.class.getName(),
+                CategoryKey.CATEGORY_CARBONFIBERS);
 
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 
