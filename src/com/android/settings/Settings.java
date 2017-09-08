5e3657844d478987dc9d218b815282c27a52f6fa
Joe Maples <joe@frap129.org>
2017-09-08 14:05:31 -0500

    Settings: Integrate CarbonFibers into Settings
    Based on both DU's Nougat and Omni's Oreo implimentations, so
    credits to mazda and maxwen for this.
    
    Android 9 edit: include circular background
    
    Android 11 edit: Jackeagle <jackeagle102@gmail.com> -Adapt to Android R Changes
    Change-Id: Id4e5367bab5826bfc8bced5684fc2e599f8140c4
    
    Change-Id: Ibaa554612a546825103da6adee1de59d8e168f3a

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -240,4 +240,8 @@ public class Settings extends SettingsActivity {
      */
     public static class MediaControlsSettingsActivity extends SettingsActivity {}
 
+    /**
+     * Activity for CarbonFibers
+     */
+    public static class CarbonFibersActivity extends SettingsActivity { /* empty */ }
 }
