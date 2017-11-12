33755f3fa6d5655a7434c64a6bbd7bb41e1ceffe
Chris <chris@cypheros.co>
2017-11-12 18:21:49 -0500

    Settings: Fix NPE for light time (Seekbar)
    Custom light on/off is causing a NPE when dependent on custom light
    enable
    
    Change-Id: Ib7cf211aa6fdb3ec6b09cd02df69959d86225c74

--- src/com/android/settings/notification/ChannelNotificationSettings.java
+++ src/com/android/settings/notification/ChannelNotificationSettings.java
@@ -232,8 +232,8 @@ public class ChannelNotificationSettings extends NotificationSettingsBase {
         mLights.setChecked(mChannel.shouldShowLights());
         //enable custom light prefs is light is enabled
         mCustomLight.setEnabled(!mLights.isDisabledByAdmin() && mChannel.shouldShowLights());
-        mLightOnTime.setEnabled(!mLights.isDisabledByAdmin() && mChannel.shouldShowLights());
-        mLightOffTime.setEnabled(!mLights.isDisabledByAdmin() && mChannel.shouldShowLights());
+        //mLightOnTime.setEnabled(!mLights.isDisabledByAdmin() && mChannel.shouldShowLights());
+        //mLightOffTime.setEnabled(!mLights.isDisabledByAdmin() && mChannel.shouldShowLights());
         mLightOnZen.setEnabled(!mLights.isDisabledByAdmin() && mChannel.shouldShowLights());
 
         //light pref
