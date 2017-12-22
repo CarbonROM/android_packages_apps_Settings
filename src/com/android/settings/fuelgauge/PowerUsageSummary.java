4d5b50308c1664955d4072a0b51321d9551a65c2
maxwen <max.weninger@gmail.com>
2017-12-22 15:15:51 +0100

    Settings: remove duplicate ambient display under battery
    Change-Id: I91800975be46375d3dd13e9b3441195064a8bbcc

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -109,7 +109,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
 
     //private static final String KEY_AUTO_BRIGHTNESS = "auto_brightness_battery";
     //private static final String KEY_SCREEN_TIMEOUT = "screen_timeout_battery";
-    private static final String KEY_AMBIENT_DISPLAY = "ambient_display_battery";
+    //private static final String KEY_AMBIENT_DISPLAY = "ambient_display_battery";
     private static final String KEY_BATTERY_SAVER_SUMMARY = "battery_saver_summary";
     private static final String KEY_HIGH_USAGE = "high_usage";
 
@@ -338,17 +338,16 @@ public class PowerUsageSummary extends PowerUsageBase implements
     }
 
     @Override
-<<<<<<< HEAD
     protected List<AbstractPreferenceController> getPreferenceControllers(Context context) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         //controllers.add(new AutoBrightnessPreferenceController(context, KEY_AUTO_BRIGHTNESS));
         //controllers.add(new TimeoutPreferenceController(context, KEY_SCREEN_TIMEOUT));
         controllers.add(new BatterySaverController(context, getLifecycle()));
         controllers.add(new BatteryPercentagePreferenceController(context));
-        controllers.add(new AmbientDisplayPreferenceController(
+        /*controllers.add(new AmbientDisplayPreferenceController(
                 context,
                 new AmbientDisplayConfiguration(context),
-                KEY_AMBIENT_DISPLAY));
+                KEY_AMBIENT_DISPLAY));*/
         controllers.add(new BatteryLightPreferenceController(context, getLifecycle()));
         return controllers;
     }
@@ -1009,7 +1008,7 @@ public class PowerUsageSummary extends PowerUsageBase implements
                     // Duplicates in display
                     //niks.add(KEY_AUTO_BRIGHTNESS);
                     //niks.add(KEY_SCREEN_TIMEOUT);
-                    niks.add(KEY_AMBIENT_DISPLAY);
+                    //niks.add(KEY_AMBIENT_DISPLAY);
                     return niks;
                 }
             };
