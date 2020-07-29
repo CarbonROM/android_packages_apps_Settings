AquariOS/packages_apps_Settings

Build with SmartAction library and supporting settings source
Change-Id: If03be1dbdbe0dc9ac0f2fb94af9d3b9a6f2162c6

Android.mk
@@ -22,6 +22,7 @@ LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SRC_FILES += $(call all-java-files-under, ../CoralReef/src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../SmartActionSettings/src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx-constraintlayout_constraintlayout \
@@ -36,7 +37,8 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     com.google.android.material_material \
     setupcompat \
     setupdesign \
-    VendorSupportLib
+    VendorSupportLib \
+    SmartActions-core
 
 LOCAL_JAVA_LIBRARIES := \
     telephony-common \
@@ -55,7 +57,8 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     zxing-core-1.7
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
-    packages/apps/CoralReef/res
+    packages/apps/CoralReef/res \
+    packages/apps/SmartActionSettings/res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
@@ -75,6 +78,9 @@ endif
 
 # AquariOS manifest
 LOCAL_FULL_LIBS_MANIFEST_FILES := packages/apps/CoralReef/AndroidManifest.xml
+
+# SmartActionSettings activities
+LOCAL_FULL_LIBS_MANIFEST_FILES += packages/apps/SmartActionSettings/AndroidManifest.xml
 
 include frameworks/base/packages/SettingsLib/common.mk
 include frameworks/base/packages/SettingsLib/search/common.mk

CarbonROM/android_packages_apps_Settings

Android.mk
@@ -22,6 +22,7 @@ LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_SRC_FILES += $(call all-java-files-under, ../CarbonFibers/src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../SmartActions/settings_src/src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx-constraintlayout_constraintlayout \
@@ -35,7 +36,8 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.recyclerview_recyclerview \
     com.google.android.material_material \
     setupcompat \
-    setupdesign
+    setupdesign \
+    SmartActions-core
 
 LOCAL_JAVA_LIBRARIES := \
     telephony-common \
@@ -54,18 +56,24 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     zxing-core-1.7
 
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
-    packages/apps/CarbonFibers/res
+    packages/apps/CarbonFibers/res \
+    packages/apps/SmartActions/settings_res
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay \
-    --extra-packages org.carbonrom.carbonfibers
+    --extra-packages org.carbonrom.carbonfibers \
+    --extra-packages com.android.smartactions
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
     LOCAL_JACK_ENABLED := incremental
     LOCAL_JACK_FLAGS := --multi-dex native
 endif
+
+
+# SmartActionSettings activities
+LOCAL_FULL_LIBS_MANIFEST_FILES += packages/apps/SmartActions/SmartActionSettings_manifest.xml
 
 include frameworks/base/packages/SettingsLib/common.mk
 include frameworks/base/packages/SettingsLib/search/common.mk