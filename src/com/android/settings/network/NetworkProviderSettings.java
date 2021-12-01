0063fb7e5358ac836ab0d71a9e1049c957c0ea15
Jacob McSwain <jacob@mcswain.dev>
2021-11-30 18:13:00 -0600

    NetworkProviderSettings: Fixup uninitialized mWifiManager
    Fixes: https://app.raygun.com/crashreporting/28qmr3e/errors/103302098654
    
    Change-Id: I33764afe7663fcff68cc2056f5c2ae5c1f114cbd

--- src/com/android/settings/network/NetworkProviderSettings.java
+++ src/com/android/settings/network/NetworkProviderSettings.java
@@ -271,6 +271,7 @@ public class NetworkProviderSettings extends RestrictedSettingsFragment
         if (activity == null) {
             return;
         }
+        mWifiManager = activity.getSystemService(WifiManager.class);
 
         if (mIsInSetupWizard) {
             GlifPreferenceLayout layout = (GlifPreferenceLayout) view;
@@ -285,7 +286,6 @@ public class NetworkProviderSettings extends RestrictedSettingsFragment
         setPinnedHeaderView(R.layout.progress_header);
         setProgressBarVisible(false);
 
-        mWifiManager = activity.getSystemService(WifiManager.class);
         if (mWifiManager != null) {
             setLoading(true, false);
             mIsViewLoading = true;
